621e1939eb538f78af0447b0d8e6b4fa045a36eb
Yoctoboy <alexis.reymann@telecom-paristech.fr>
2020-02-01 23:33:21 +0100

    custol debounce function

--- static/js/sliders.js
+++ static/js/sliders.js
@@ -1,12 +1,28 @@
-update_led_strip = _.debounce(
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+
+update_led_strip = debounce(
     function(slider, slidertype){
         $.ajax({
             url: `/update/${slidertype}/${slider.val()}`,
             type: 'PUT',
         })
     },
-    wait=0.5,
-    options={leading:true}
+    500,
+    false
 )
 
 
