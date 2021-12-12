84dea86187566faa68bda142ff4fbb87062c5d11
Yoctoboy <alexis@padam.io>
2021-12-12 16:25:55 +0100

    new project structure and babel dependency

---
+++ frontend/src/colorModeComponents/StaticGradient.component.tsx
@@ -0,0 +1,5 @@
+function StaticGradient() {
+  return <div className="Other">LOL</div>;
+}
+
+export default StaticGradient;
---
+++ frontend/src/network.ts
@@ -0,0 +1,13 @@
+import request from 'superagent';
+
+const backendBaseUrl = 'http://192.168.1.48:8000/';
+
+export const updatePlainColor = (hexColor = '#000000') => {
+  console.log('');
+
+  return request
+    .post(`${backendBaseUrl}setPlainColor`)
+    .send({ color: hexColor })
+    .set('Accept', 'application/json');
+  //return request.get(`${backendBaseUrl}hello`).query({}).set('Accept', 'application/json')
+};
